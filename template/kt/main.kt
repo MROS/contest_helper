0851e53eb09276fe4b96d878e3f97000b71dca71
MROS <yc1043@gmail.com>
2020-05-23 20:28:52 +0800

    新增 kotlin 輸入模板

---
+++ template/kt/main.kt
@@ -0,0 +1,7 @@
+private fun readLn() = readLine()!!
+private fun readInt() = readLn().toInt()
+private fun readStrings() = readLn().split(" ")
+private fun readInts() = readStrings().map { it.toInt() }
+
+fun main() {
+}
